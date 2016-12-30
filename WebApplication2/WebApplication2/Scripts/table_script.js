ad43f3064343bf8425b3b434f7c7fa00acb1a6b7
damifil <damifil15@gmail.com>
2016-12-30 17:39:30 +0100

    poprawa tabel
    poprawiłem tabele w grupyi statystyki tabeli w zaklad na razie nie
    ruszam bo dawid tm pracuje

---
+++ WebApplication2/WebApplication2/Scripts/table_script.js
@@ -0,0 +1,47 @@
+﻿table_script = {
+
+    init: function () {
+        this.initDataTable();
+    },
+
+    initDataTable: function () {
+        var table = $('#tabela_ranking').DataTable({
+
+        });
+
+        table_script.returnDataTable = function () {
+            return table;
+        }
+    },
+
+
+    initDataTable: function () {
+        var table = $('#tabela_ranking').DataTable({
+            "language": {
+                "sProcessing": "Przetwarzanie...",
+                "sLengthMenu": "Pokaż _MENU_ pozycji",
+                "sZeroRecords": "Nie znaleziono pasujących pozycji",
+                "sInfoThousands": " ",
+                "sInfo": "Pozycje od _START_ do _END_ z _TOTAL_ łącznie",
+                "sInfoEmpty": "Pozycji 0 z 0 dostępnych",
+                "sInfoFiltered": "(filtrowanie spośród _MAX_ dostępnych pozycji)",
+                "sInfoPostFix": "",
+                "sSearch": "Szukaj:",
+                "sUrl": "",
+                "oPaginate": {
+                    "sFirst": "Pierwsza",
+                    "sPrevious": "Poprzednia",
+                    "sNext": "Następna",
+                    "sLast": "Ostatnia"
+                }
+            }
+        });
+    }
+
+};
+
+$(function () {
+    table_script.init();
+});
+
+
