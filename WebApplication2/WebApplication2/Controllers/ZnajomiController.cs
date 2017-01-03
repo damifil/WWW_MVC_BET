2e080d3455077df997a29dc4437b390ffca5ed6f
DawidWikt <dawidnfs@gmail.com>
2017-01-03 14:30:49 +0100

    dodano wyświetlenie znajomych

--- WebApplication2/WebApplication2/Controllers/ZnajomiController.cs
+++ WebApplication2/WebApplication2/Controllers/ZnajomiController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication2.Models.DB;
+using WebApplication2.Models.ViewModel;
 
 namespace WebApplication2.Controllers
 {
@@ -11,7 +13,20 @@ namespace WebApplication2.Controllers
         // GET: Znajomi
         public ActionResult Index()
         {
-            return View();
+            ProjektEntities db = new ProjektEntities();
+            var friend = from i in db.FRIENDS
+                         where i.User_ID == User.Identity.Name
+                         join us in db.USER
+                         on i.Friend_ID equals us.User_ID
+                         select new { us.User_ID, us.Image };
+            var mod = new List<FriendsView>();
+            foreach (var x in friend)
+            {
+                mod.Add(new FriendsView { UserID = x.User_ID, imageData = x.Image });
+            }
+
+            return View(mod);
+
         }
     }
 }
---
+++ WebApplication2/WebApplication2/Models/ViewModel/FriendsView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models.ViewModel
+{
+    public class FriendsView
+    {
+        public string UserID { get; set; }
+        public byte[] imageData { get; set; }
+        public FriendsView()
+        {
+            this.UserID = null;
+            this.imageData = null;
+        }
+    }
+}
