6764e5dada0912ac7f93de0096adffe9893171c8
damifil <damifil15@gmail.com>
2017-01-19 02:25:21 +0100

    dziala odbieranie i wysylaniwe waidomosci poprawione css-y poprawione layouty
    -nie dziala obrazek w 2 layoucie

--- WebApplication2/WebApplication2/Controllers/GrupyController.cs
+++ WebApplication2/WebApplication2/Controllers/GrupyController.cs
@@ -9,15 +9,18 @@ using WebApplication2.Models.ViewModel;
 
 namespace WebApplication2.Controllers
 {
+    public static class MyStaticValues
+    {
+        public static string groupname { get; set; }
+    }
+
     public class GrupyController : Controller
     {
         // GET: Grupy
         public ActionResult Index(string groupName)
         {
             ProjektEntities db = new ProjektEntities();
-            System.Diagnostics.Debug.WriteLine(groupName);
-            System.Diagnostics.Debug.WriteLine("po nazwiegrupyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy");
-
+            MyStaticValues.groupname=groupName;
             var message = from m in db.Group_mesage
                           where (m.Group_Name == groupName)
                           orderby m.Group_chat_ID
@@ -74,7 +77,7 @@ namespace WebApplication2.Controllers
             };
 
             var message = from m in db.Group_mesage
-                          where (m.Group_Name == "as")
+                          where (m.Group_Name == MyStaticValues.groupname)
                           orderby m.Group_chat_ID
                           select new { m.from_user, m.message, m.Group_Name };
 
@@ -84,23 +87,22 @@ namespace WebApplication2.Controllers
                 {
                     fromUser = item.from_user,
                     message = item.message,
-                    groupname = item.Group_Name,                    
+                    groupname = item.Group_Name,
                 });
             }
 
 
             Group_mesage newMessage = new Group_mesage();
-             newMessage.from_user = User.Identity.Name;
-             newMessage.message= messageListGroupView.newMessageContent;
-             newMessage.Group_Name= "as";
-             
+            newMessage.from_user = User.Identity.Name;
+            newMessage.message = messageListGroupView.newMessageContent;
+            newMessage.Group_Name = MyStaticValues.groupname;
 
-         
+
 
             db.Group_mesage.Add(newMessage);
             db.SaveChanges();
 
-            return RedirectToAction("Index", new { groupName = "as" });
+            return RedirectToAction("Index", new { groupName = MyStaticValues.groupname });
         }
     }
 
